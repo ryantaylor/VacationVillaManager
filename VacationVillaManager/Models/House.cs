03f33b0538f69767291139100d8035fe03910b68
ryantaylor <ryan.taylor9@gmail.com>
2013-05-08 10:47:29 -0400

    Added city field to house data.

--- VacationVillaManager/Models/House.cs
+++ VacationVillaManager/Models/House.cs
@@ -15,6 +15,7 @@ namespace VacationVillaManager.Models
 
         public string Name { get; set; }
         public string Address { get; set; }
+        public string City { get; set; }
         public string Region { get; set; }
         public string Country { get; set; }
         public string ZipPostalCode { get; set; }
