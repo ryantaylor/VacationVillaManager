12b7dd661147cf5fa3810d30d6ed498fae38fad8
ryantaylor <ryan.taylor9@gmail.com>
2013-06-18 01:17:32 -0400

    Another name change.

--- VacationVillaManager/App_Start/BundleConfig.cs
+++ VacationVillaManager/App_Start/BundleConfig.cs
@@ -41,7 +41,7 @@ namespace VacationVillaManager
 
             // Bootstrap bundles
 
-            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+            bundles.Add(new ScriptBundle("~/bundles/scripts-bootstrap").Include(
                 "~/Scripts/jquery-{version}.js",
                 "~/Scripts/jquery-migrate-{version}.js",
                 "~/Scripts/bootstrap.js",
