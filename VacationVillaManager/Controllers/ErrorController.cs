e3639004007ab8d425166cf9b450b7dfed5315ca
ryantaylor <ryan.taylor9@gmail.com>
2013-08-06 10:33:15 -0400

    Added custom 404 and changed newsletter wording.

---
+++ VacationVillaManager/Controllers/ErrorController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VacationVillaManager.Controllers
+{
+    public class ErrorController : BootstrapBaseController
+    {
+        //
+        // GET: /Error/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View();
+        }
+    }
+}
